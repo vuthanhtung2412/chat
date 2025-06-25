adf53d784fdbe5f9b62dae12ef77917231eccfe5
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-06-26 00:30:01 +0200

    from ngrok to tailscale funnel

--- front/src/const.ts
+++ front/src/const.ts
@@ -1,5 +1,8 @@
-export const BACKEND_URL = "http://localhost:4000/chat"
-export const PROD_BACKEND_URL = "https://tough-apt-stag.ngrok-free.app/chat"
-export const WS_URL = "ws://localhost:4000/chat/socket/websocket"
-export const PROD_WS_URL = "ws://tough-apt-stag.ngrok-free.app/socket/websocket"
+const HOST = "localhost:4000"
+// const PROD_HOST = "tough-apt-stag.ngrok-free.app"
+const PROD_HOST = "tung-ps42-modern-8ra.barking-duckbill.ts.net"
 export const GH_PROJECT = "chat"
+export const BACKEND_URL = `http://${HOST}/${GH_PROJECT}`
+export const PROD_BACKEND_URL = `https://${PROD_HOST}/${GH_PROJECT}`
+export const WS_URL = `ws://${HOST}/${GH_PROJECT}/socket/websocket`
+export const PROD_WS_URL = `ws://${PROD_HOST}/${GH_PROJECT}/socket/websocket`
